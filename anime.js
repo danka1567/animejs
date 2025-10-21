77759b80a8d0e92d80780b277899c342ad135c73
john banega don <narutoanimeinspirationquote@gmail.com>
2025-10-21 13:04:32 +0600

    Update anime.js

--- anime.js
+++ anime.js
@@ -1,185 +1,50 @@
-// Anime database with episode mapping
-const animeDatabase = [
-    {
-        "name": "road-of-naruto-18220?",
-        "tmdb_id": null,
-        "year": "N/A",
-        "episodes": "1-12",
-        "episode_offset": 0
-    },
-    {
-        "name": "naruto-find-the-crimson-four-leaf-clover-5694?",
-        "tmdb_id": null,
-        "year": "N/A",
-        "episodes": "1-25",
-        "episode_offset": 12
-    },
-    {
-        "name": "naruto-the-movie-3-guardians-of-the-crescent-moon-kingdom-4005?",
-        "tmdb_id": null,
-        "year": "N/A",
-        "episodes": "1-67",
-        "episode_offset": 37
-    },
-    {
-        "name": "naruto-the-movie-2-legend-of-the-stone-of-gelel-4004?",
-        "tmdb_id": null,
-        "year": "N/A",
-        "episodes": "1-3",
-        "episode_offset": 104
-    },
-    {
-        "name": "naruto-the-cross-roads-4291?",
-        "tmdb_id": null,
-        "year": "N/A",
-        "episodes": "1-10",
-        "episode_offset": 107
-    },
-    {
-        "name": "naruto-ova2-the-lost-story-mission-protect-the-waterfall-village-4538?",
-        "tmdb_id": null,
-        "year": "N/A",
-        "episodes": "1-4",
-        "episode_offset": 117
-    },
-    {
-        "name": "naruto-narutimate-hero-3-tsuini-gekitotsu-jounin-vs-genin-musabetsu-dairansen-taikai-kaisai-4485?",
-        "tmdb_id": null,
-        "year": "N/A",
-        "episodes": "1-6",
-        "episode_offset": 121
-    },
-    {
-        "name": "naruto-ova3-hidden-leaf-village-grand-sports-festival-4136?",
-        "tmdb_id": null,
-        "year": "N/A",
-        "episodes": "1-52",
-        "episode_offset": 127
-    },
-    {
-        "name": "boruto-naruto-the-movie-1391?",
-        "tmdb_id": null,
-        "year": "N/A",
-        "episodes": "1-3",
-        "episode_offset": 179
-    },
-    {
-        "name": "naruto-x-ut-1840?",
-        "tmdb_id": null,
-        "year": "N/A",
-        "episodes": "1-16",
-        "episode_offset": 182
-    },
-    {
-        "name": "naruto-shippuuden-movie-4-the-lost-tower-1821?",
-        "tmdb_id": null,
-        "year": "N/A",
-        "episodes": "1-11",
-        "episode_offset": 198
-    },
-    {
-        "name": "naruto-ova9-sunny-side-battle-1916?",
-        "tmdb_id": null,
-        "year": "N/A",
-        "episodes": "1-12",
-        "episode_offset": 209
-    },
-    {
-        "name": "naruto-shippuuden-movie-5-blood-prison-1642?",
-        "tmdb_id": null,
-        "year": "N/A",
-        "episodes": "1-24",
-        "episode_offset": 221
-    },
-    {
-        "name": "naruto-shippuuden-movie-3-inheritors-of-will-of-fire-2044?",
-        "tmdb_id": null,
-        "year": "N/A",
-        "episodes": "1-4",
-        "episode_offset": 245
-    },
-    {
-        "name": "boruto-naruto-the-movie-the-day-naruto-became-the-hokage-1805?",
-        "tmdb_id": null,
-        "year": "N/A",
-        "episodes": "1-4",
-        "episode_offset": 249
-    },
-    {
-        "name": "naruto-the-movie-ninja-clash-in-the-land-of-snow-3162?",
-        "tmdb_id": null,
-        "year": "N/A",
-        "episodes": "1-12",
-        "episode_offset": 253
-    },
-    {
-        "name": "naruto-ova5-naruto-the-genie-and-the-three-wishes-3657?",
-        "tmdb_id": null,
-        "year": "N/A",
-        "episodes": "1-11",
-        "episode_offset": 265
-    },
-    {
-        "name": "naruto-spin-off-rock-lee-his-ninja-pals-2992?",
-        "tmdb_id": 43168,
-        "year": "2012",
-        "episodes": "1-51",
-        "episode_offset": 276
-    },
-    {
-        "name": "naruto-shippuden-the-movie-2-bonds-2346?",
-        "tmdb_id": null,
-        "year": "N/A",
-        "episodes": "1-13",
-        "episode_offset": 327
-    },
-    {
-        "name": "naruto-shippuden-the-movie-2306?",
-        "tmdb_id": null,
-        "year": "N/A",
-        "episodes": "1-12",
-        "episode_offset": 340
-    },
-    {
-        "name": "naruto-ova7-chunin-exam-on-fire-and-naruto-vs-konohamaru-2928?",
-        "tmdb_id": null,
-        "year": "N/A",
-        "episodes": "1-24",
-        "episode_offset": 352
-    },
-    {
-        "name": "naruto-shippuden-355?",
-        "tmdb_id": 31910,
-        "year": "2007",
-        "episodes": "1-500",
-        "episode_offset": 7882
-    },
-    {
-        "name": "the-last-naruto-the-movie-882?",
-        "tmdb_id": null,
-        "year": "N/A",
-        "episodes": "1-13",
-        "episode_offset": 876
-    },
-    {
-        "name": "naruto-677?",
-        "tmdb_id": 46260,
-        "year": "2002",
-        "episodes": "1-220",
-        "episode_offset": 12352
-    },
-    {
-        "name": "naruto-shippuuden-movie-6-road-to-ninja-1066?",
-        "tmdb_id": null,
-        "year": "N/A",
-        "episodes": "1-12",
-        "episode_offset": 1109
-    },
-    {
-        "name": "boruto-naruto-next-generations-8143?",
-        "tmdb_id": 70881,
-        "year": "2017",
-        "episodes": "1-293",
-        "episode_offset": 1121
-    }
-]
+[
+            {
+                "name": "fullmetal-alchemist-brotherhood-1?",
+                "tmdb_id": 25425,
+                "year": 2009,
+                "episodes": "1-64",
+                "episode_offset": 0 // No offset needed
+            },
+            {
+                "name": "hunter-x-hunter-2?",
+                "tmdb_id": 46298,
+                "year": 2011,
+                "episodes": "65-212",
+                "episode_offset": 64 // Episode 1 in TMDB becomes episode 65 in MegaPlay
+            },
+            {
+                "name": "steinsgate-3?",
+                "tmdb_id": 42509,
+                "year": 2011,
+                "episodes": "213-236",
+                "episode_offset": 212 // Episode 1 in TMDB becomes episode 213 in MegaPlay
+            },
+
+             {
+                "name": "naruto-shippuden-355?",
+                "tmdb_id": 31910,
+                "year": 2007,
+                "episodes": "7882-8381",
+                "episode_offset": 7881 // Episode 1 in TMDB becomes episode 213 in MegaPlay
+            },
+
+
+                {
+                "name": "naruto?",
+                "tmdb_id": 42509,
+                "year": 2011,
+                "episodes": "213-236",
+                "episode_offset": 212 // Episode 1 in TMDB becomes episode 213 in MegaPlay
+            },
+
+
+                {
+                "name": "one piece?",
+                "tmdb_id": 42509,
+                "year": 2011,
+                "episodes": "213-236",
+                "episode_offset": 212 // Episode 1 in TMDB becomes episode 213 in MegaPlay
+            }
+        ];
+        
